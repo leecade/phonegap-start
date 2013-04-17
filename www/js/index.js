36f6312e8246ca9bc65a8a36e65a56290d5c9655
leecade <leecade@163.com>
2013-04-17 12:54:24 +0800

    test

--- www/js/index.js
+++ www/js/index.js
@@ -22,6 +22,20 @@ var app = {
     },
     bind: function() {
         document.addEventListener('deviceready', this.deviceready, false);
+
+
+
+        // DEBUG
+        var element = document.getElementById('deviceProperties');
+
+            element.innerHTML = 'Device Name: '     + device.name     + '<br />' + 
+                                'Device Cordova: '  + device.cordova + '<br />' + 
+                                'Device Platform: ' + device.platform + '<br />' + 
+                                'Device UUID: '     + device.uuid     + '<br />' + 
+                                'Device Model: '    + device.model     + '<br />' + 
+                                'Device Version: '  + device.version  + '<br />';
+
+
     },
     deviceready: function() {
         // This is an event handler function, which means the scope is the event.
